9e5a08961097dc11e845cad57f708828cf02017b
Amir Jafari <amir@mindly.ee>
2020-07-16 17:29:10 +0300

    change the card name

--- src/js/connector.js
+++ src/js/connector.js
@@ -1,7 +1,7 @@
 console.log("hello world!");
 window.TrelloPowerUp.initialize({
   "card-badges": function (t, opts) {
-    return t.card('all')
+    return t.card('name')
     .then(function(card) {
       console.log(card)
       return[{
