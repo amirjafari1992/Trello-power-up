bb80c73c342a6ffebb64b8a0ba2f41e6933c9089
Amir Jafari <amir@mindly.ee>
2020-07-16 17:34:17 +0300

    changing the settings

--- src/js/connector.js
+++ src/js/connector.js
@@ -1,7 +1,7 @@
 console.log("hello world!");
 window.TrelloPowerUp.initialize({
-  "card-badges": function (t, opts) {
-    return t.card('name')
+  "attachment-sections": function (t, opts) {
+    return t.card('all')
     .then(function(card) {
       console.log(card)
       return[{
