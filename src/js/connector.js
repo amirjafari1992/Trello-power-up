b59b7d97bad2640a5aa1f882a180e4756d5e8636
Amir Jafari <amir@mindly.ee>
2020-07-16 17:38:09 +0300

    test

--- src/js/connector.js
+++ src/js/connector.js
@@ -1,6 +1,6 @@
 console.log("hello world!");
 window.TrelloPowerUp.initialize({
-  "attachment-sections": function (t, opts) {
+  "card-detail-badges": function (t, opts) {
     return t.card('all')
     .then(function(card) {
       console.log(card)
