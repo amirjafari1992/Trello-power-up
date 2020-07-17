d44a972831f13ab5b8ec3a8d5f6e0a3dc0d45e4c
Amir Jafari <amir@mindly.ee>
2020-07-17 12:25:51 +0300

    adding a box message

--- src/js/connector.js
+++ src/js/connector.js
@@ -15,7 +15,10 @@ var GRAY_ICON =
   "https://cdn.hyperdev.com/us-east-1%3A3d31b21c-01a0-4da2-8827-4bc6e88b7618%2Ficon-gray.svg";
 
 var onBtnClick = function (t, opts) {
-  console.log("Someone clicked the button");
+  var point = prompt("Please enter your point", "0");
+  if (point != null) {
+    alert(`your point is ${point}`)
+  }
 };
 
 window.TrelloPowerUp.initialize({
