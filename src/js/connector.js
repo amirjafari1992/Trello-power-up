b8961312a8a47faf19658d656d79e74e08dacf08
Amir Jafari <amir@mindly.ee>
2020-07-17 10:45:06 +0300

    changing the functionality

--- src/js/connector.js
+++ src/js/connector.js
@@ -1,12 +1,38 @@
-console.log("hello world!");
+// console.log("hello world!");
+// window.TrelloPowerUp.initialize({
+//   "card-badges": function (t, opts) {
+//     return t.card('all')
+//     .then(function(card) {
+//       console.log(card)
+//       return[{
+//         text: card.idShort
+//       }]
+//     })
+//   },
+// });
+
+var GRAY_ICON = 'https://cdn.hyperdev.com/us-east-1%3A3d31b21c-01a0-4da2-8827-4bc6e88b7618%2Ficon-gray.svg';
+
+var onBtnClick = function (t, opts) {
+  console.log('Someone clicked the button');
+};
+
 window.TrelloPowerUp.initialize({
-  "card-badges": function (t, opts) {
-    return t.card('all')
-    .then(function(card) {
-      console.log(card)
-      return[{
-        text: card.idShort
-      }]
-    })
-  },
+  'card-buttons': function (t, opts) {
+    return [{
+      // usually you will provide a callback function to be run on button click
+      // we recommend that you use a popup on click generally
+      icon: GRAY_ICON, // don't use a colored icon here
+      text: 'Open Popup',
+      callback: onBtnClick,
+      condition: 'edit'
+    }, {
+      // but of course, you could also just kick off to a url if that's your thing
+      icon: GRAY_ICON,
+      text: 'Just a URL',
+      condition: 'always',
+      url: 'https://developer.atlassian.com/cloud/trello',
+      target: 'Trello Developer Site' // optional target for above url
+    }];
+  }
 });
