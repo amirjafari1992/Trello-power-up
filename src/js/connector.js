b8a08e7bd4a6a841bdf6fd1980e9db8e092df754
Amir Jafari <amir@mindly.ee>
2020-07-17 12:20:39 +0300

    changes1

--- src/js/connector.js
+++ src/js/connector.js
@@ -11,11 +11,24 @@ console.log("hello world!");
 //   },
 // });
 
-TrelloPowerUp.initialize({
-  'card-buttons': function(t, options){
-    return [{
-      icon: 'https://cdn.glitch.com/1b42d7fe-bda8-4af8-a6c8-eff0cea9e08a%2Frocket-ship.png?1494946700421',
-      text: 'Estimate Size',
-    }];
+var GRAY_ICON =
+  "https://cdn.hyperdev.com/us-east-1%3A3d31b21c-01a0-4da2-8827-4bc6e88b7618%2Ficon-gray.svg";
+
+var onBtnClick = function (t, opts) {
+  console.log("Someone clicked the button");
+};
+
+window.TrelloPowerUp.initialize({
+  "card-buttons": function (t, opts) {
+    return [
+      {
+        // usually you will provide a callback function to be run on button click
+        // we recommend that you use a popup on click generally
+        icon: GRAY_ICON, // don't use a colored icon here
+        text: "Perkimator Point",
+        callback: onBtnClick,
+        condition: "edit",
+      },
+    ];
   },
 });
