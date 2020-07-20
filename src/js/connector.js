41549f58208b372e2bc0726c71c438f4487fcc2a
Amir Jafari <amir@mindly.ee>
2020-07-20 13:30:13 +0300

    fetching data

--- src/js/connector.js
+++ src/js/connector.js
@@ -4,30 +4,56 @@ var GRAY_ICON =
 
 var ApiKey = "";
 var CardID = "";
+async function postData(url = "", data = {}) {
+  // Default options are marked with *
+  const response = await fetch(url, {
+    method: "POST", // *GET, POST, PUT, DELETE, etc.
+    mode: "cors", // no-cors, *cors, same-origin
+    cache: "no-cache", // *default, no-cache, reload, force-cache, only-if-cached
+    credentials: "same-origin", // include, *same-origin, omit
+    headers: {
+      "Content-Type": "application/json",
+      // 'Content-Type': 'application/x-www-form-urlencoded',
+    },
+    redirect: "follow", // manual, *follow, error
+    referrerPolicy: "no-referrer", // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
+    body: JSON.stringify(data), // body data type must match "Content-Type" header
+  });
+  return response.json(); // parses JSON response into native JavaScript objects
+}
 
 var onBtnClick = function (t, opts) {
-  return Promise.all([t.get("board", "private", "perkiPoint")]).spread(function(perkiPoint){
-    if(perkiPoint != null) {
-      var point = prompt("Please enter your point", perkiPoint);
-    } else {
-      var point = prompt("Please enter your point", "0");
+  return Promise.all([t.get("board", "private", "perkiPoint")]).spread(
+    function (perkiPoint) {
+      if (perkiPoint != null) {
+        var point = prompt("Please enter your point", perkiPoint);
+      } else {
+        var point = prompt("Please enter your point", "0");
+      }
+      if (point != null) {
+        return Promise.all([t.get("board", "private", "apiKey")])
+          .spread(function (key) {
+            if (key != null) {
+              return t.card("all").then(function (card) {
+                data = {
+                  cardId: card.id,
+                  storyPoints: point
+                }
+                postData(`https://beta.perkimator.com/powerup?apiKey=${key}`, data).then(
+                  (response) => {
+                    console.log(response); 
+                  }
+                );
+                return t.set("board", "private", "perkiPoint", point);
+              });
+            } else {
+              alert("You must add your API KEY frist!");
+            }
+          })
+          .then(function () {});
+      }
     }
-    if (point != null) {
-      return Promise.all([t.get("board", "private", "apiKey")])
-        .spread(function (key) {
-          if (key != null) {
-            ApiKey = key;
-            return t.card("all").then(function (card) {
-              CardID = card.id;
-              return t.set('board', 'private', 'perkiPoint', point)
-            });
-          } else {
-            alert("You must add you API KEY frist!");
-          }
-        })
-        .then(function () {});
-    }
-  })
+  );
 };
 
 window.TrelloPowerUp.initialize({
