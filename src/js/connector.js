d15c3e129c130c4786fc30a3ef5c32bfb3a08722
Amir Jafari <amir@mindly.ee>
2020-07-20 16:46:03 +0300

    fetch changing

--- src/js/connector.js
+++ src/js/connector.js
@@ -25,8 +25,17 @@ var onBtnClick = function (t, opts) {
               fetch(
                 `https://beta.perkimator.com/callback/powerup?apiKey=${key}`,
                 {
-                  method: "POST",
-                  body: JSON.stringify(data),
+                  method: "POST", // *GET, POST, PUT, DELETE, etc.
+                  mode: "cors", // no-cors, *cors, same-origin
+                  cache: "no-cache", // *default, no-cache, reload, force-cache, only-if-cached
+                  credentials: "same-origin", // include, *same-origin, omit
+                  headers: {
+                    "Content-Type": "application/json",
+                    // 'Content-Type': 'application/x-www-form-urlencoded',
+                  },
+                  redirect: "follow", // manual, *follow, error
+                  referrerPolicy: "no-referrer", // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
+                  body: JSON.stringify(data), // body data type must match "Content-Type" header
                 }
               );
             });
@@ -55,7 +64,7 @@ window.TrelloPowerUp.initialize({
       return [
         {
           text: perkiPoint,
-          color: 'purple'
+          color: "purple",
         },
       ];
     });
