91c6c77af16d839c9e8f9fa78769b3945a48985c
Amir Jafari <amir@mindly.ee>
2020-07-20 10:45:52 +0300

    new

--- src/js/connector.js
+++ src/js/connector.js
@@ -34,7 +34,7 @@ window.TrelloPowerUp.initialize({
   'show-settings': function(t, options){
 		return t.popup({
 			title: 'Settings',
-			url: 'https://google.com,
+			url: 'https://google.com',
 			height: 184
 		});
 	},
