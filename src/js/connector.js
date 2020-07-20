0730a4f5e004305fc30f6bee07a5d2a94aba0894
Amir Jafari <amir@mindly.ee>
2020-07-20 11:25:00 +0300

    get the apiKey

--- src/js/connector.js
+++ src/js/connector.js
@@ -1,14 +1,14 @@
+var Promise = TrelloPowerUp.Promise;
 var GRAY_ICON =
   "https://cdn.hyperdev.com/us-east-1%3A3d31b21c-01a0-4da2-8827-4bc6e88b7618%2Ficon-gray.svg";
 
 var onBtnClick = function (t, opts) {
   var point = prompt("Please enter your point", "0");
   if (point != null) {
-    alert(`your point is ${point}`);
+    alert(t.get('board', 'private', 'apiKey'));
   }
 };
 
-var Promise = TrelloPowerUp.Promise;
 
 window.TrelloPowerUp.initialize({
   "card-buttons": function (t, opts) {
