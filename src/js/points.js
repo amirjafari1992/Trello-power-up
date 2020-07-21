4dca3d810c0bf567069bf0c4666a767293e0f4cf
Amir Jafari <amir@mindly.ee>
2020-07-21 15:18:22 +0300

    t4

--- src/js/points.js
+++ src/js/points.js
@@ -21,21 +21,21 @@ button.addEventListener("click", function () {
     return Promise.all([t.get("board", "private", "apiKey")])
       .spread(function (key) {
         if (key != null) {
-          t.set("card", "private", {"perkiPoint": point});
+          t.set("card", "private", "perkiPoint", inputField.value);
 
-        //   t.card("all").then(function (card) {
-        //     data = {
-        //       cardId: card.id,
-        //       storyPoints: point,
-        //     };
-        //     axios
-        //       .post(
-        //         `https://beta.perkimator.com/callback/powerup?apiKey=546ccf19-36ed-4117-9faf-4d10eb00c0a5`,
-        //         data
-        //       )
-        //       .then(function (response) {})
-        //       .catch(function (error) {});
-        //   });
+          t.card("all").then(function (card) {
+            data = {
+              cardId: card.id,
+              storyPoints: point,
+            };
+            axios
+              .post(
+                `https://beta.perkimator.com/callback/powerup?apiKey=546ccf19-36ed-4117-9faf-4d10eb00c0a5`,
+                data
+              )
+              .then(function (response) {})
+              .catch(function (error) {});
+          });
         } else {
           alert("You must add your API KEY frist!");
         }
