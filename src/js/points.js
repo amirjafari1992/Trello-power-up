41b5032d9b21527c040aa39ae25f9fa8e86d4c39
Amir Jafari <amir@mindly.ee>
2020-07-21 14:53:07 +0300

    changes2

---
+++ src/js/points.js
@@ -0,0 +1,42 @@
+var inputField = document.getElementById("pointInput");
+var button = document.getElementById("savePoint");
+
+t.render(function () {
+  return Promise.all([t.get("card", "private", "perkiPoint")]).spread(function (
+    perkiPoint
+  ) {
+    if (perkiPoint != null) {
+      inputField.value = perkiPoint;
+    } else {
+      inputField.value = "0";
+    }
+  });
+});
+
+button.addEventListener("click", function () {
+  if (inputField.value != null) {
+    var data = "";
+    return Promise.all([t.get("board", "private", "apiKey")])
+      .spread(function (key) {
+        if (key != null) {
+          t.set("card", "private", "perkiPoint", point);
+          t.card("all").then(function (card) {
+            data = {
+              cardId: card.id,
+              storyPoints: point,
+            };
+            axios
+              .post(
+                `https://beta.perkimator.com/callback/powerup?apiKey=546ccf19-36ed-4117-9faf-4d10eb00c0a5`,
+                data
+              )
+              .then(function (response) {})
+              .catch(function (error) {});
+          });
+        } else {
+          alert("You must add your API KEY frist!");
+        }
+      })
+      .then(function () {});
+  }
+});
