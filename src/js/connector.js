72694101769cc9cf8ad0d3807d1f1fe32d9ca495
Amir Jafari <amir@mindly.ee>
2020-07-21 14:08:39 +0300

    first commit

--- src/js/connector.js
+++ src/js/connector.js
@@ -63,7 +63,7 @@ window.TrelloPowerUp.initialize({
     return t.get("card", "private", "perkiPoint").then(function (perkiPoint) {
       return [
         {
-          title: 'Perkimator Point',
+          title: 'Perks',
           text: perkiPoint,
           color: "purple",
         },
